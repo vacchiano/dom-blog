87dff9f88e48f44d63617fe3bc05cca01240fc5f
Dom Vacchiano <djvacchiano@gmail.com>
2022-01-20 09:40:08 -0600

    added 2 posts and add them to home page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,18 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +24,18 @@ export default function Home() {
           Hey, I'm Dom. I'm a technical co-founder working on new features at BiopharmIQ. Feel free to connect on
           {' '}<a href="https://twitter.com/domvacchiano" target="_blank">Twitter</a>.
         </p>
+      </section>
+      <section>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title} <br />
+              {id} <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
